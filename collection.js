e16ab92344d3909b1acb2466cd2ddb467435a448
RANJITH K <146610886+RANJITH19KUMAR@users.noreply.github.com>
2025-09-09 21:04:18 +0530

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,21 @@
+var productContainer=document.getElementById("product")
+var  search=document.getElementById("search")
+var productList=productContainer.querySelectorAll(".petname")
+
+search.addEventListener("keyup",function(event){
+    var enteredValue = event.target.value.toUpperCase()
+
+    for( var count=0;count<productList.length;count=count+1)
+
+        {
+            var productname=productList[count].querySelector("p").textContent
+            if(productname.toUpperCase().indexOf(enteredValue)<0)
+            {
+                productList[count].style.display="none"
+            }
+            else
+            {
+                productList[count].style.display="block"
+            }
+        }
+})
